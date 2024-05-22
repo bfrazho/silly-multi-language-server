98544affe67be619dd9f75be5878789aeb60d372
Brian <befrazho@gmail.com>
2024-05-21 23:40:05 -0400

    moved tests to user folder, adjusted injection strategy to pass around application state

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,16 +7,28 @@ use axum::Router;
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 struct AppState {
     pub user_base_url: String
+}
+impl AppState{
+    pub fn set_user_base_url(&mut self, user_base_url: &str)-> &Self{
+        self.user_base_url = user_base_url.to_string();
+        self
+    }
+    pub fn to_owned(&self)-> Self {
+        self.clone()
+    }
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
     
-    let app = Router::new().merge(routes("https://reqres.in/api".to_string()));
+    let app = Router::new().merge(routes(&AppState::default()
+        .set_user_base_url("https://reqres.in/api")
+    ));
+
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
@@ -26,77 +38,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-fn routes(user_base_url: String) -> Router {
-    let app_state = AppState{user_base_url: user_base_url};
+fn routes(app_state: &AppState) -> Router {
     Router::new().merge(users::routes(app_state))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::{response_converter::ResponseBody,
-        users::UserResponse,
-    };
-    use axum::{body::Body, http::Request};
-    use httpmock::{Method::POST, MockServer};
-    use hyper::{header, Method, StatusCode};
-    use serde_json::json;
-    use tower::ServiceExt;
-
-    #[tokio::test]
-    async fn local_post_endpoint_calls_reqres_post_endpoint() {
-        let routes = routes("https://reqres.in/api".to_string());
-
-        let response = routes
-            .oneshot(
-                Request::builder()
-                    .method(Method::POST)
-                    .uri("/users")
-                    .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-                    .body(Body::from(
-                        serde_json::to_string(&json!({"name": "someone", "job": "role"})).unwrap(),
-                    ))
-                    .unwrap(),
-            )
-            .await
-            .unwrap();
-
-        assert_eq!(StatusCode::OK, response.status());
-
-        let user_response: UserResponse = response.get_json().await.unwrap();
-        assert_eq!(user_response.get_name(), "someone");
-        assert_eq!(user_response.get_job(), "role");
-        assert!(i64::from_str_radix(user_response.get_id(), 10).is_ok());
-    }
-
-    #[tokio::test]
-    async fn mockserver_create_user() {
-        let mock_server = MockServer::start();
-        let mock = mock_server.mock(|when, then|{
-            when.method(POST)
-                .path("/users")
-                .json_body(json!({"name": "the name", "job": "the role"}));
-            then.status(200)
-                .json_body(json!({"name": "the name", "job": "the role", "id": "the id"}));
-        });
-        let routes = routes(mock_server.base_url());
-
-        let response = routes
-        .oneshot(
-            Request::builder()
-                .method(Method::POST)
-                .uri("/users")
-                .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-                .body(Body::from(
-                    serde_json::to_string(&json!({"name": "the name", "job": "the role"})).unwrap(),
-                ))
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-        mock.assert();
-        let user_response: UserResponse = response.get_json().await.unwrap();
-        assert_eq!(UserResponse::new("the name".to_string(), "the role".to_string(), "the id".to_string()), user_response);
-    }
-}
--- rust/src/users.rs
+++ rust/src/users.rs
@@ -39,13 +39,11 @@ pub struct UserRequest {
     job: String
 }
 
-pub fn routes(app_state: AppState)-> Router{
+pub fn routes(app_state: &AppState)-> Router{
     Router::new()
         .route("/users", post(create_user))
-        .with_state(app_state)
+        .with_state(app_state.to_owned())
 }
-
-
 
 async fn create_user(
     State(AppState{user_base_url}): State<AppState>,
@@ -57,4 +55,75 @@ async fn create_user(
         .send()
         .await.context("failed to call post on https://reqres.in/api/users")?;
     Ok(Json(response.get_json().await?))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{routes, AppState};
+    use crate::{response_converter::ResponseBody,
+        users::UserResponse,
+    };
+    use axum::{body::Body, http::Request};
+    use httpmock::{Method::POST, MockServer};
+    use hyper::{header, Method, StatusCode};
+    use serde_json::json;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn local_post_endpoint_calls_reqres_post_endpoint() {
+        let routes = routes(AppState::default().set_user_base_url("https://reqres.in/api"));
+
+        let response = routes
+            .oneshot(
+                Request::builder()
+                    .method(Method::POST)
+                    .uri("/users")
+                    .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(
+                        serde_json::to_string(&json!({"name": "someone", "job": "role"})).unwrap(),
+                    ))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(StatusCode::OK, response.status());
+
+        let user_response: UserResponse = response.get_json().await.unwrap();
+        assert_eq!(user_response.get_name(), "someone");
+        assert_eq!(user_response.get_job(), "role");
+        assert!(i64::from_str_radix(user_response.get_id(), 10).is_ok());
+    }
+
+    #[tokio::test]
+    async fn mockserver_create_user() {
+        let input_json = json!({"name": "the name", "job": "the role"});
+        let mock_server = MockServer::start();
+        let mock = mock_server.mock(|when, then|{
+            when.method(POST)
+                .path("/users")
+                .json_body(input_json.clone());
+            then.status(200)
+                .json_body(json!({"name": "the name", "job": "the role", "id": "the id"}));
+        });
+        let routes = routes(AppState::default().set_user_base_url(&mock_server.base_url()));
+
+        let response = routes
+        .oneshot(
+            Request::builder()
+                .method(Method::POST)
+                .uri("/users")
+                .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .body(Body::from(
+                    serde_json::to_string(&input_json).unwrap(),
+                ))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+        mock.assert();
+        let user_response: UserResponse = response.get_json().await.unwrap();
+        assert_eq!(UserResponse::new("the name".to_string(), "the role".to_string(), "the id".to_string()), user_response);
+    }
 }

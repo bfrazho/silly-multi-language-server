59f8af7942a6f9ceeac98fa394faaeb49c979a14
Brian <befrazho@gmail.com>
2024-05-21 23:19:19 -0400

    added mock test and injected base url for user endpoint

--- rust/src/users.rs
+++ rust/src/users.rs
@@ -1,10 +1,10 @@
 use anyhow::Context;
-use axum::{routing::post, Json, Router};
+use axum::{extract::State, routing::post, Json, Router};
 use serde::{Deserialize, Serialize};
 
-use crate::{app_error::AppError, response_converter::ResponseBody};
+use crate::{app_error::AppError, response_converter::ResponseBody, AppState};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct UserResponse {
     name: String,
     job: String,
@@ -26,7 +26,11 @@ pub mod test {
         pub fn get_id(&self)-> &str {
             &self.id
         }
+        pub fn new(name: String, job: String, id: String)-> Self{
+            Self{name, job, id}
+        }
     }
+
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -35,14 +39,20 @@ pub struct UserRequest {
     job: String
 }
 
-pub fn routes()-> Router{
+pub fn routes(app_state: AppState)-> Router{
     Router::new()
         .route("/users", post(create_user))
+        .with_state(app_state)
 }
 
-async fn create_user(Json(user_request): Json<UserRequest>) -> Result<Json<UserResponse>, AppError> {
+
+
+async fn create_user(
+    State(AppState{user_base_url}): State<AppState>,
+    Json(user_request): Json<UserRequest>,
+) -> Result<Json<UserResponse>, AppError> {
     let client = reqwest::Client::new();
-    let response = client.post("https://reqres.in/api/users")
+    let response = client.post(format!("{}/users", user_base_url))
         .json(&user_request)
         .send()
         .await.context("failed to call post on https://reqres.in/api/users")?;
